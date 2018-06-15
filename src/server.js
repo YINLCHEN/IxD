f0a94414ea402b13141200220cfa4a1d3e1e26ac
陳胤霖 <sidneychen137@gmail.com>
2018-06-15 16:20:05 +0800

    Ver 3.0 UAT 2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3001;
 
@@ -9,7 +9,7 @@ server.listen(port, () => {
     console.log('Server listening at port %d', port);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('build'));
 
 var RoomList = {};
 var curRoomName = '0';
